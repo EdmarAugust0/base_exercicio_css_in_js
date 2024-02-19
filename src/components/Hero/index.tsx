dbbf25f829b3e471d82594f06d6c314cdee0741b
Edmar <edmar.contato149@gmail.com>
2024-02-19 10:08:52 -0300

    adicionando a estilização styled components ao projeto

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,13 @@
-import styles from './Hero.module.css'
+// import styles from './Hero.module.css'
+
+import { Hero as HeroContainer } from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
+  <HeroContainer>
     <div className="container">
-      <h2 className={styles.heroTitle}>
-        As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
+      <h2>As melhores vagas para tecnologia, design e artes visuais.</h2>
     </div>
-  </form>
+  </HeroContainer>
 )
 
 export default Hero
